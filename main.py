79d3ee72a1bd897a4be0bef8852b924f3f0bc4e1
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2025-05-22 17:54:27 +0900

    Feat: [안성희] "업로드된 파일 객체 확장자에 따라 customer_id 검증하기"

--- main.py
+++ main.py
@@ -15,3 +15,31 @@ from fastapi import UploadFile, File
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     return {"filename" : file.filename}
+
+import pandas as pd
+from io import StringIO, BytesIO
+
+@app.post("/upload-file/")
+async def upload_check_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    file_extension = file.filename.split(".")[-1].lower()
+    
+    if file_extension == "csv":
+        # CSV 파일 처리
+        s = StringIO(contents.decode('utf-8'))
+        df = pd.read_csv(s)
+    elif file_extension in ["xlsx", "xls"]:
+        # Excel 파일 처리
+        excel_data = BytesIO(contents)
+        df = pd.read_excel(excel_data)
+    else:
+        return {"error": "지원되지 않는 파일 형식입니다. CSV 또는 Excel 파일만 업로드해주세요."}
+    
+    # 헤더 확인
+    first_column_check = df.columns[0] == 'customer_id'
+        
+    
+    return {
+        "filename": file.filename,
+        "first_column_is_customer_id": first_column_check
+    }
