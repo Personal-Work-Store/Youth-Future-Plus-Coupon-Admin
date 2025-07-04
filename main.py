3be4dc7bfc404193b192ebffabe697e1ebddc5f4
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2025-07-04 18:08:31 +0900

    chore: [안성희] "main.py 주석 추가"

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 app = FastAPI()
 
-# LocalStack S3 클라이언트 설정
+# LocalStack S3 클라이언트 : Presigned URL 생성 등 특수 기능은 client에서만 지원
 s3_client = boto3.client(
     "s3",
     aws_access_key_id="test",
@@ -17,7 +17,7 @@ s3_client = boto3.client(
     endpoint_url="http://localhost:4566",
 )
 
-# DynamoDB 리소스 (로컬스택 환경)
+# DynamoDB 리소스 : 테이블 아이템등 리소스 단위로 작업하는 경우가 많아 resource 방식 사용
 dynamodb = boto3.resource(
     "dynamodb",
     endpoint_url="http://localhost:4566",
@@ -25,10 +25,11 @@ dynamodb = boto3.resource(
     aws_access_key_id="test",
     aws_secret_access_key="test",
 )
+# FileMetadata 테이블 객체 할당 해당 테이블이 없을경우 table 객체를 만들때는 오류가 일어나지 않지만 읽거나 쓰기 작업을 할 때 오류가 발생
 table = dynamodb.Table("FileMetadata")
 
 
-# 메타데이터 모델
+# Pydantic의 BaesModel을 상속받아 데이터 모델로 동작
 class FileMetadata(BaseModel):
     filename: str
     size: int
@@ -39,7 +40,9 @@ class FileMetadata(BaseModel):
 # Presigned URL 발급
 @app.post("/presigned-url/")
 async def get_presigned_url(metadata: FileMetadata):
+    # 사용할 S3 bucket 이름
     bucket_name = "sample-bucket"
+    # 업로드할 S3 오브젝트의 경로(키)를 파일명 기반으로 생성
     key = f"uploads/{metadata.filename}"
     try:
         presigned_put_url = s3_client.generate_presigned_url(
@@ -51,14 +54,8 @@ async def get_presigned_url(metadata: FileMetadata):
             },
             ExpiresIn=3600,
         )
-        presigned_get_url = s3_client.generate_presigned_url(
-            ClientMethod="get_object",
-            Params={"Bucket": bucket_name, "Key": key},
-            ExpiresIn=3600,
-        )
         return {
             "upload_url": presigned_put_url,
-            "download_url": presigned_get_url,
             "s3_key": key,
         }
     except Exception as e:
@@ -70,11 +67,17 @@ async def get_presigned_url(metadata: FileMetadata):
 def save_metadata(metadata: FileMetadata):
     file_id = str(uuid.uuid4())
     item = {
+        # 파일 고유 식별자
         "file_id": file_id,
+        # 파일 이름
         "filename": metadata.filename,
+        # S3 버킷에 저장될 경로
         "s3_key": f"uploads/{metadata.filename}",
+        # 파일 크기
         "size": metadata.size,
+        # 파일의 MIME 타입
         "content_type": metadata.content_type,
+        # 업로드 시각
         "uploaded_at": metadata.uploaded_at or datetime.utcnow().isoformat(),
     }
     try:
@@ -99,8 +102,11 @@ def validate_s3_csv(info: S3FileInfo):
     를 검증합니다.
     """
     try:
+        # Bucket의 Key 파일의 바이너리 스트림과 메타데이터를 response에 저장
         response = s3_client.get_object(Bucket=info.bucket, Key=info.key)
+        # 바이너리스트림을 텍스트 스트림으로 변환
         stream = io.TextIOWrapper(response["Body"], encoding="utf-8")
+        # CSV 파일을 한 줄씩 읽을 준비
         reader = csv.reader(stream)
         # 헤더 검증
         try:
